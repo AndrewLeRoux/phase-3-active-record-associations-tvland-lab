96ea8d2be7aeffc934a9bb1ce42d27cd77b10e14
Andrew LeRoux <andrewleroux2639@gmail.com>
2022-03-22 12:10:11 -0700

    commit tree e5c288be9dd4868c8017780ca76ef83647c5c13c

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    arr = []
+    characters = self.characters
+    for character in characters do
+        arr.append("#{character.name} - #{character.show.name}")
+    end
+    arr
+  end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    actors = self.actors
+    arr = []
+    for actor in actors do
+        arr.append(actor.full_name)
+    end
+    arr
+  end
 end
